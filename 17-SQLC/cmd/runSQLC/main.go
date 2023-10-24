614f3d5f22914e21c968add021cce2f4cb244d0f
Christian Ferraz <christianferraz@gmail.com>
2023-10-24 00:22:56 -0400

    uow

--- 17-SQLC/cmd/runSQLC/main.go
+++ 17-SQLC/cmd/runSQLC/main.go
@@ -3,9 +3,81 @@ package main
 import (
 	"context"
 	"database/sql"
+	"fmt"
 
-	"github.com/christianferraz/goexpert/17-SQLC/db"
+	"github.com/christianferraz/goexpert/17-SQLC/internal/db"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type CourseDB struct {
+	dbConn *sql.DB
+	*db.Queries
+}
+
+func NewCourseDB(dbConn *sql.DB) *CourseDB {
+	return &CourseDB{
+		dbConn:  dbConn,
+		Queries: db.New(dbConn),
+	}
+}
+
+type CourseParams struct {
+	ID          string
+	Name        string
+	Description sql.NullString
+	Price       float64
+}
+
+type CategoryParams struct {
+	ID          string
+	Name        string
+	Description sql.NullString
+}
+
+func (c *CourseDB) callTx(ctx context.Context, fn func(*db.Queries) error) error {
+	tx, err := c.dbConn.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	q := db.New(tx)
+	err = fn(q)
+	if err != nil {
+		if errRb := tx.Rollback(); errRb != nil {
+			return fmt.Errorf("error on rollback: %v, original error: %w", errRb, err)
+		}
+		return err
+	}
+	return tx.Commit()
+}
+
+func (c *CourseDB) CreateCourseAndCategory(ctx context.Context, argsCategory CategoryParams, argsCourse CourseParams) error {
+	err := c.callTx(ctx, func(q *db.Queries) error {
+		var err error
+		err = q.CreateCategory(ctx, db.CreateCategoryParams{
+			ID:          argsCategory.ID,
+			Name:        argsCategory.Name,
+			Description: argsCategory.Description,
+		})
+		if err != nil {
+			return err
+		}
+		err = q.CreateCourse(ctx, db.CreateCourseParams{
+			ID:          argsCourse.ID,
+			Name:        argsCourse.Name,
+			Description: argsCourse.Description,
+			CategoryID:  argsCategory.ID,
+			Price:       argsCourse.Price,
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func main() {
 	ctx := context.Background()
@@ -17,42 +89,28 @@ func main() {
 
 	queries := db.New(dbConn)
 
-	// err = queries.CreateCategory(ctx, db.CreateCategoryParams{
-	//     ID: uuid.New().String(),
-	//     Name: "Backend",
-	//     Description: sql.NullString{String: "Backend description", Valid: true},
-	// })
-
+	// courseArgs := CourseParams{
+	// 	ID:          uuid.New().String(),
+	// 	Name:        "Go",
+	// 	Description: sql.NullString{String: "Go course", Valid: true},
+	// }
+	// categoryArgs := CategoryParams{
+	// 	ID:          uuid.New().String(),
+	// 	Name:        "Backend",
+	// 	Description: sql.NullString{String: "Backend Course", Valid: true},
+	// }
+	// courseDB := NewCourseDB(dbConn)
+	// err = courseDB.CreateCourseAndCategory(ctx, categoryArgs, courseArgs)
 	// if err != nil {
-	//     panic(err)
+	// 	panic(err)
 	// }
 
-	// categories, err := queries.ListCategories(ctx)
-	// if err != nil {
-	//     panic(err)
-	// }
-
-	// for _, category := range categories {
-	//     println(category.ID, category.Name, category.Description.String)
-	// }
-
-	// err = queries.UpdateCategory(ctx, db.UpdateCategoryParams{
-	//     ID: "361d935e-4ed1-49c3-afad-bdf5da95bbc9",
-	//     Name: "Backend updated",
-	//     Description: sql.NullString{String: "Backend description updated", Valid: true},
-	// })
-
-	err = queries.DeleteCategory(ctx, "361d935e-4ed1-49c3-afad-bdf5da95bbc9")
+	courses, err := queries.ListCourses(ctx)
 	if err != nil {
 		panic(err)
 	}
-
-	categories, err := queries.ListCategories(ctx)
-	if err != nil {
-		panic(err)
+	for _, c := range courses {
+		fmt.Printf("Category: %s, Course ID: %s, Course Name: %s, Course Description: %s, Course Price: %f", c.CategoryID, c.ID, c.Name, c.Description.String, c.Price)
 	}
 
-	for _, category := range categories {
-		println(category.ID, category.Name, category.Description.String)
-	}
 }
