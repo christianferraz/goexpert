2ae4ad5fff7cd6118a13506ad6b310f0d8c832c8
Christian Ferraz <christianferraz@gmail.com>
2023-10-30 18:59:31 -0400

    Product handler

--- 9-APIs/internal/webserver/handler/product_handler.go
+++ 9-APIs/internal/webserver/handler/product_handler.go
@@ -7,6 +7,8 @@ import (
 	"github.com/christianferraz/goexpert/9-APIs/internal/dto"
 	"github.com/christianferraz/goexpert/9-APIs/internal/entity"
 	"github.com/christianferraz/goexpert/9-APIs/internal/infra/database"
+	entityPkg "github.com/christianferraz/goexpert/9-APIs/pkg/entity"
+	"github.com/go-chi/chi/v5"
 )
 
 type ProductHandler struct {
@@ -37,4 +39,76 @@ func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *ProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	product, err := h.productDB.FindByID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&product)
+}
+
+func (h *ProductHandler) GetProducts(w http.ResponseWriter, r *http.Request) {
+	page := r.URL.Query().Get("page")
+	if page == "" {
+		w.WriteHeader(http.StatusExpectationFailed)
+		return
+	}
+}
+
+func (h *ProductHandler) UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	var product entity.Product
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	product.ID, err = entityPkg.ParseID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	_, err = h.productDB.FindByID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	err = h.productDB.Update(&product)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *ProductHandler) DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	_, err := h.productDB.FindByID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	err = h.productDB.Delete(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
