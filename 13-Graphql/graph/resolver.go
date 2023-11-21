a0f35f98f678d55a000502d1c8c570fde51eae9c
Christian Ferraz <christianferraz@gmail.com>
2023-11-21 01:12:52 -0400

    Graphql

---
+++ 13-Graphql/graph/resolver.go
@@ -0,0 +1,12 @@
+package graph
+
+import "github.com/christianferraz/goexpert/13-Graphql/internal/database"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CategoryDB *database.Category
+	CoursesDB  *database.Course
+}
---
+++ 7-Packaging/4/sistema/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/christianferraz/goexpert/7-Packaging/4/math"
+	"github.com/google/uuid"
+)
+
+func main() {
+	m := math.NewMath(1, 2)
+	println(m.Add())
+	println(uuid.New().String())
+}
