68e9c1c0fb345741324cae573d7f280657220057
Christian Ferraz <christianferraz@gmail.com>
2024-01-06 11:36:32 -0400

    alura

--- pdf/main.go
+++ pdf/main.go
@@ -6,35 +6,6 @@
 
 package main
 
-import (
-	"fmt"
-
-	"github.com/dslipak/pdf"
-)
-
 func main() {
-	f, err := pdf.Open("input.pdf")
-	// remember close file
-	defer f.Close()
-	if err != nil {
-		return "", err
-	}
-	totalPage := r.NumPage()
-
-	for pageIndex := 1; pageIndex <= totalPage; pageIndex++ {
-		p := r.Page(pageIndex)
-		if p.V.IsNull() {
-			continue
-		}
-		var lastTextStyle pdf.Text
-		texts := p.Content().Text
-		for _, text := range texts {
-			if isSameSentence(text, lastTextStyle) {
-				lastTextStyle.S = lastTextStyle.S + text.S
-			} else {
-				fmt.Printf("Font: %s, Font-size: %f, x: %f, y: %f, content: %s \n", lastTextStyle.Font, lastTextStyle.FontSize, lastTextStyle.X, lastTextStyle.Y, lastTextStyle.S)
-				lastTextStyle = text
-			}
-		}
-	}
+	println("Hello, World!")
 }
