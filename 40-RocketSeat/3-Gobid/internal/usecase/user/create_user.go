40e9d7c89a3074124abd9fac861e79e9c97e4d1a
Christian Ferraz <christianferraz@gmail.com>
2025-01-28 01:11:05 -0400

    email regex

--- 40-RocketSeat/3-Gobid/internal/usecase/user/create_user.go
+++ 40-RocketSeat/3-Gobid/internal/usecase/user/create_user.go
@@ -20,5 +20,6 @@ func (req CreateUserReq) Valid(ctx context.Context) validator.Evaluator {
 	eval.CheckField(validator.NotBlank(req.Bio), "bio", "This field is not empty")
 	eval.CheckField(validator.MinChars(req.UserName, 10) && validator.MaxChars(req.UserName, 255), "username", "This field must have a length between 10 and 255 characters")
 	eval.CheckField(validator.MinChars(req.PasswordHash, 8), "password_hash", "This field must have a length greater than 8 characters")
+	eval.CheckField(validator.Matches(req.Email, validator.EmailRX), "email", "This field must be a valid email")
 	return eval
 }
