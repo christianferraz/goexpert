163aa16d2c0b50025e7f2312cf5cdfb524f121a2
Christian Ferraz <christianferraz@gmail.com>
2025-02-13 22:45:30 -0400

    inicio taskfy

---
+++ 40-RocketSeat/4-Taskfy/cmd/api/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"encoding/gob"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/alexedwards/scs/pgxstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/christianferraz/goexpert/40-RocketSeat/3-Gobid/internal/api"
+	"github.com/christianferraz/goexpert/40-RocketSeat/3-Gobid/internal/services"
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {
+	// if err := godotenv.Load(); err != nil {
+	// 	panic(err)
+	// }
+	gob.Register(uuid.UUID{})
+	ctx := context.Background()
+	pool, err := pgxpool.New(ctx, fmt.Sprintf("user=%s password=%s  host=%s port=%s dbname=%s sslmode=disable",
+		os.Getenv("GOBID_DATABASE_USER"),
+		os.Getenv("GOBID_DATABASE_PASSWORD"),
+		os.Getenv("GOBID_DATABASE_HOST"),
+		os.Getenv("GOBID_DATABASE_PORT"),
+		os.Getenv("GOBID_DATABASE_NAME"),
+	))
+	if err != nil {
+		panic(err)
+	}
+	defer pool.Close()
+	if err := pool.Ping(ctx); err != nil {
+		panic(err)
+	}
+	s := scs.New()
+	s.Store = pgxstore.New(pool)
+	s.Lifetime = 24 * time.Hour
+	s.Cookie.HttpOnly = true
+	s.Cookie.SameSite = http.SameSiteLaxMode
+	api := api.Api{
+		Router:         chi.NewMux(),
+		UserService:    services.NewUserService(pool),
+		ProductService: services.NewProductService(pool),
+		BidsService:    services.NewBidsService(pool),
+		Sessions:       s,
+		WSupgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		},
+		AuctionLobby: services.AuctionLobby{
+			Rooms: make(map[uuid.UUID]*services.AuctionRoom),
+		},
+	}
+	api.BindRoutes()
+	api.Start()
+}
