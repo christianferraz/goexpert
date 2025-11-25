4b90ae10f0083e9ac3665133e7f6139f9bb083cb
Christian Pacheco <christianferraz@gmail.com>
2025-11-24 22:02:54 -0400

    upgrade

--- 10-Multithreading/4-Channels/9-Buffer_2/main.go
+++ 10-Multithreading/4-Channels/9-Buffer_2/main.go
@@ -8,8 +8,11 @@ import (
 
 func rotina(ch chan<- int) {
 	ch <- 1
+	fmt.Println("executou o 1")
 	ch <- 2
+	fmt.Println("executou o 2")
 	ch <- 3
+	fmt.Println("executou o 3")
 	ch <- 4
 	ch <- 5
 	fmt.Println("Executou!")
@@ -17,15 +20,22 @@ func rotina(ch chan<- int) {
 	ch <- 6
 }
 func main() {
-	ch := make(chan int, 6)
+	ch := make(chan int, 128)
 	go rotina(ch)
 	fmt.Println(<-ch)
+	fmt.Println("recebeu o 1")
+	time.Sleep(time.Second * 5)
 	fmt.Println(<-ch)
+	fmt.Println("recebeu o 2")
 	time.Sleep(time.Second)
 	fmt.Println(<-ch)
+	fmt.Println("recebeu o 3")
 	fmt.Println(<-ch)
+	fmt.Println("recebeu o 4")
 	fmt.Println(<-ch)
+	fmt.Println("recebeu o 5")
 	fmt.Println(<-ch)
+	fmt.Println("recebeu o 6")
 	fmt.Println("nr cpus ", runtime.NumCPU())
 	close(ch)
 }
